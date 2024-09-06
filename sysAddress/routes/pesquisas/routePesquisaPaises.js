aea91bee4675b1b98eca36cdee1f8e211c080fc9
Andre Henrique de Paula Souza <andre.souza@facil.local>
2024-09-06 16:42:16 -0300

    Alteração na pesquisa de paises.

--- sysAddress/routes/pesquisas/routePesquisaPaises.js
+++ sysAddress/routes/pesquisas/routePesquisaPaises.js
@@ -2,6 +2,10 @@ function routePesquisaPaises(application) {
     application.get('/PesquisaPaises', function(request, response) {
         application.sysAddress.controllers.pesquisas.controlPesquisaPaises.viewPesquisaPaises(application, request, response);
     });
+
+    application.post('/consultaPaisesPorNome', function(request, response) {
+        application.sysAddress.controllers.pesquisas.controlPesquisaPaises.consultaPaisesPorNome(application, request, response);
+    });
 }
 
 module.exports = routePesquisaPaises;
